44ec1a8fedd57660c63a839735fd452a4a326086
m2nseo <kms9357@novatek.kr>
2020-07-21 19:14:04 +0900

    new file

--- game.c
+++ game.c
@@ -4,19 +4,36 @@
     int complay;
     int i;
     int x;
+ 
 
     int main()
     {
-        
+        printf("choose number 10 times!\n");
+        printf("N N N\nN N N\nN N N\n");
+
         for(i=0; 10 > i;i++) 
         {
+        char bord[9]={'N','N','N','N','N','N','N','N','N'};
         scanf("%d",&x );
         complay = rand()%10;
-        if(complay == x)
-        printf("%d Good!\n",complay);
-        else
-        printf("%d Bad\n",complay);
-        
+        if(complay == x) {
+            printf("Good! %d is Correct!\n",complay);
+            bord[x-1] = 'O';
+            printf("%c %c %c\n%c %c %c\n%c %c %c\n",
+            bord[0], bord[1], bord[2], 
+            bord[3], bord[4], bord[5], 
+            bord[6], bord[7], bord[8]);
+        }
+        else {
+        char bord[9]={'N','N','N','N','N','N','N','N','N'};
+        printf("Bad luck! %d was answer\n",complay);
+        bord[x-1] = 'X';
+        bord[complay-1] = '0';
+            printf("%c %c %c\n%c %c %c\n%c %c %c\n",
+            bord[0], bord[1], bord[2], 
+            bord[3], bord[4], bord[5], 
+            bord[6], bord[7], bord[8]);
+        }
         }
         return 0;
     }
