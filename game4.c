dd82711c3c82473fc3cc713fd9c09e69e613f8f0
m2nseo <kms9357@novatek.kr>
2020-07-22 11:40:18 +0900

    add file

---
+++ game4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+    
+    int complay;
+    int i;
+    int x;
+    char bord[9]={'N','N','N','N','N','N','N','N','N'};
+
+    int main()
+    {
+        printf("choose number! You Have 5 Chance!\n");
+        printf("N N N\nN N N\nN N N\n");
+
+        complay = rand()%10;
+
+        for(i=0; 5 > i;i++) 
+        {
+        scanf("%d",&x );
+        
+
+        if(complay == x) {
+            printf("Great! %d is Correct!\n",complay);
+            bord[x-1] = 'O';
+            printf("%c %c %c\n%c %c %c\n%c %c %c\n",
+            bord[0], bord[1], bord[2], 
+            bord[3], bord[4], bord[5], 
+            bord[6], bord[7], bord[8]);
+            i = 5;
+        }
+        else {
+            if(i < 4)  {
+                printf("Wrong Answer! %d Chances Left!\n", 4-i);
+                bord[x-1] = 'X';
+                printf("%c %c %c\n%c %c %c\n%c %c %c\n",
+                        bord[0], bord[1], bord[2], 
+                        bord[3], bord[4], bord[5], 
+                        bord[6], bord[7], bord[8]);
+            }
+            else
+                printf("You Lose.. Try Next Game!\n");
+        }
+        }
+        return 0;
+    }
